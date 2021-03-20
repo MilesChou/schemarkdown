59a9d1950d1f23ccb069a20f19401c01e3d16848
MilesChou <jangconan@gmail.com>
2021-03-21 03:11:25 +0800

    Add test

--- src/SchemarkdownServiceProvider.php
+++ src/SchemarkdownServiceProvider.php
@@ -28,13 +28,15 @@ class SchemarkdownServiceProvider extends ServiceProvider
 
     private function registerViews(): void
     {
-        $this->loadViewsFrom(__DIR__ . '/templates', 'schemarkdown');
-
         $this->app->afterResolving('view', function (Factory $view) {
             $view->addExtension('txt', 'text', function () {
                 return $this->app->make(TemplateEngine::class);
             });
         });
+
+        // Notice: must to register package before load views.
+        // See https://github.com/laravel/framework/compare/v6.9.0...v6.10.0#diff-bf561b2572e934630e46ff6b69a2e3e359e216f7690fad6ac9b8acaa48502fb4L90-R96
+        $this->loadViewsFrom(__DIR__ . '/templates', 'schemarkdown');
     }
 
     private function registerCommand(): void
